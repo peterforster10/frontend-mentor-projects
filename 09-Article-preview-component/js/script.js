59b4810e48096e3e9e3dbaf5a84d836defc3d21d
Peter Forster <peter.forster10@gmail.com>
2025-02-13 18:22:00 +0100

    project 09

--- 09-Article-preview-component/js/script.js
+++ 09-Article-preview-component/js/script.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+  const $shareLink = document.querySelector('.share-link');
+  const $shareTooltip = document.querySelector('.share-tooltip');
+
+  $shareLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    $shareTooltip.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!e.target.closest('.post-share')) {
+      $shareTooltip.classList.add('hidden');
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    const keyName = e.key;
+    if (keyName === 'Escape') {
+      $shareTooltip.classList.add('hidden');
+    }
+  });
 }
 
 init();
