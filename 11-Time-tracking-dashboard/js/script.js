9fd585c3d92bd0f5348560eab54e8cbb68dae900
Peter Forster <peter.forster10@gmail.com>
2025-02-16 10:45:57 +0100

    project 11

--- 11-Time-tracking-dashboard/js/script.js
+++ 11-Time-tracking-dashboard/js/script.js
@@ -1,12 +1,53 @@
 'use strict';
 
+let state = {
+  currentPeriod: 'weekly',
+};
 
-const init = async () => {
-  const userData = await loadUserData();
-  console.log(userData);
-  initUser(userData);
+const periodLabels = {
+  daily: 'Day',
+  weekly: 'Week',
+  monthly: 'Month',
 }
 
+const init = async () => {
+  initPeriodNav();
+  await initUser();
+  await initTimeFrameCards();
+}
+
+const initPeriodNav = () => {
+  const $periodNavLinks = document.querySelectorAll('.period-nav .nav-link');
+  for (const $navLink of $periodNavLinks) {
+    $navLink.addEventListener('click', handleNavClick);
+  }
+}
+
+const handleNavClick = async (e) => {
+  e.preventDefault();
+  const $navLink = e.target;
+  const newPeriod = $navLink.dataset.period;
+  state.currentPeriod = newPeriod;
+  updatePeriodNav();
+  await initTimeFrameCards();
+}
+
+const updatePeriodNav = () => {
+  const $periodNavLinks = document.querySelectorAll('.period-nav .nav-link');
+  for (const $navLink of $periodNavLinks) {
+    $navLink.classList.remove('active');
+    if ($navLink.dataset.period === state.currentPeriod) {
+      $navLink.classList.add('active');
+    }
+  }
+}
+
+const initUser = async () => {
+
+  const userData = await loadUserData();
+  console.log(userData);
+  updateUser(userData);
+}
 
 const loadUserData = async () => {
   const response = await fetch('user_data.json');
@@ -14,7 +55,7 @@ const loadUserData = async () => {
   return userData;
 }
 
-const initUser = (userData) => {
+const updateUser = (userData) => {
   const $userImgBox = document.querySelector('.user-img-box');
   const $userImg = document.createElement('img');
   const $userFullName = document.querySelector('.user-full-name');
@@ -27,6 +68,37 @@ const initUser = (userData) => {
   $userImgBox.append($userImg);
 
   $userFullName.textContent = fullName;
+}
+
+const initTimeFrameCards = async () => {
+  const timeFrameData = await loadTimeframeData();
+  console.log(timeFrameData);
+  if (timeFrameData) {
+    for (const timeFrameItem of timeFrameData) {
+      updateTimeFrameCard(timeFrameItem);
+    }
+  }
+}
+
+const loadTimeframeData = async () => {
+  const response = await fetch('timeframe_data.json');
+  const timeFrameData = await response.json();
+  return timeFrameData;
+}
+
+const updateTimeFrameCard = (timeFrameItem) => {
+  const { title } = timeFrameItem;
+  const cardTitle = title.toLowerCase().replace(' ', '-');
+  
+  const $card = document.querySelector(`.${cardTitle}-card`);
+  const $cardHours = $card.querySelector('.card-hours');
+  const $lastPeriod = $card.querySelector('.last-period');
+
+
+  const timeFrame = timeFrameItem.timeframes[state.currentPeriod];
+
+  $cardHours.textContent = `${timeFrame.current} hrs`;
+  $lastPeriod.textContent = `Last ${periodLabels[state.currentPeriod]} - ${timeFrame.previous} hrs`;
 }
 
 init();
